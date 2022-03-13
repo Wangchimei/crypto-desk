c8ebc9a2b517ac8daf5b4404103f09fd03e7a88a
Chi Mei Wang <kimmy0929@gmail.com>
2022-03-13 22:42:30 +0900

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </div>
   );
 }
